59ae8a28df3e948368998bddc9b32d427bc04000
bang <bang@ubuntu.(none)>
2015-01-18 20:33:58 +0800

    adjust css

--- db/schema.rb
+++ db/schema.rb
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 20141227080143) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email" # unique
+    t.string   "email"
     t.string   "passwd"
     t.string   "level"
     t.string   "name"
