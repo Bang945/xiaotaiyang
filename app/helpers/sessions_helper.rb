a3243bd18710498f69e7dc46ba39f48a6015921d
sichenchang <sichenfdu@gmail.com>
2015-01-18 05:17:20 -0800

    addlogin

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
-end
+
+	 def sign_in(user)    
+	  remember_token = User.new_remember_token  
+
+	   cookies.permanent[:remember_token] = remember_token    
+	   user.update_attribute(:remember_token, User.hash(remember_token))
+	   #更新单个属性并保存到数据库   
+	   self.current_user= user
+	 end 
+
+	 def current_user=(user)    
+	  @current_user = user 
+	 end 
+end 
+
